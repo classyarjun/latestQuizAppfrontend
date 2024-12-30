a5b93c2ae3b8fd326f4a81ec83334086ef8e8a1f
Arjun Rajput <arjunrajput7531@gmail.com>
2024-12-30 14:58:40 +0530

    sucesspage changed

--- src/Environment/environment.production.ts
+++ src/Environment/environment.production.ts
@@ -1,11 +1,9 @@
 
-
 //? Update this with your production API or settings
 export const environment = {
     production: true,
     // apiUrl: '/api'
     apiUrl:'http://localhost:8080/api'
-
   };
 
 
